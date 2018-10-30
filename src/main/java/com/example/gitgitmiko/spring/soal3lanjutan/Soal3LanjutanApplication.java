1807a255ff4c2177b929e13e46973c6bb609baba
GITGIT <sjatmiko@gmail.com>
2018-10-30 15:29:55 +0700

    Initial commit

---
+++ src/main/java/com/example/gitgitmiko/spring/soal3lanjutan/Soal3LanjutanApplication.java
@@ -0,0 +1,12 @@
+package com.example.gitgitmiko.spring.soal3lanjutan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soal3LanjutanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soal3LanjutanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/gitgitmiko/spring/soal3lanjutan/Soal3LanjutanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.gitgitmiko.spring.soal3lanjutan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Soal3LanjutanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
